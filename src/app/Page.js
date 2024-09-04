af72bdf7b12ff48af8a5fd534c88786ba1f779dd
Jejelove Graphics <89115888+jGraphics@users.noreply.github.com>
2024-09-04 01:37:33 -0700

    Create Page.js

---
+++ src/app/Page.js
@@ -0,0 +1,19 @@
+import HomeSection from './components/HomeSection';
+import AboutSection from './components/AboutSection';
+import WorkSection from './components/WorkSection';
+import NewsSection from './components/NewsSection';
+import ContactSection from './components/ContactSection';
+
+const Home = () => {
+  return (
+    <>
+      <HomeSection />
+      <AboutSection />
+      <WorkSection />
+      <NewsSection />
+      <ContactSection />
+    </>
+  );
+};
+
+export default Home;
